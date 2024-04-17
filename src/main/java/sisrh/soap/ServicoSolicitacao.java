e59a7a3a4c8114a94ef818c7b09bfe9ae09723f3
Matheus Ortunes <eu.matheusortunes@gmail.com>
2024-04-17 15:14:06 -0300

    reseting project

---
+++ src/main/java/sisrh/soap/ServicoSolicitacao.java
@@ -0,0 +1,39 @@
+package sisrh.soap;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.xml.ws.WebServiceContext;
+
+import sisrh.banco.Banco;
+import sisrh.dto.Empregado;
+import sisrh.dto.Empregados;
+import sisrh.seguranca.Autenticador;
+
+@WebService
+@SOAPBinding(style = Style.RPC)
+public class ServicoSolicitacao {
+
+    @Resource
+    WebServiceContext context;
+    private Empregado emp;
+
+    @WebMethod(action = "solicitar")
+    public Empregados solicitar() throws Exception {
+
+        Autenticador.autenticarUsuarioSenha(context);
+
+        Solicitacoes solicitacoes = new Solicitacoes();
+
+        List<Empregado> lista = Banco.listarEmpregados();
+        for(Empregado emp: lista) {
+            empregados.getEmpregados().add(emp);
+        }
+        return empregados;
+    }
+
+}
